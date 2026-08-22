4f24182d8e2a9e90abaffbfaf8a170719aa062b1
sfmict <15520489+sfmict@users.noreply.github.com>
2026-08-22 15:32:01 +0000

    Fixed SexyMap grabbing in the classic

--- HidingBar/HidingBar.lua
+++ HidingBar/HidingBar.lua
@@ -505,7 +505,8 @@ function hb:ADDON_LOADED(addonName)
 			self.db.config = nil
 		end
 
-		C_Timer.After(0, function()
+		-- 1.1 is a fix for SexyMap grabbing
+		C_Timer.After(WOW_PROJECT_MAINLINE == WOW_PROJECT_ID and 0 or 1.1, function()
 			xpcall(self.setProfile, CallErrorHandler, self)
 			self.cb:Fire("INIT")
 			self.init = nil
